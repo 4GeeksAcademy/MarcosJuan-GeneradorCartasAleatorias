838273862363d1969faae33ab8135e631de4e735
Marcos Juan Martos Prados <martospradosmarcos@gmail.com>
2024-11-26 16:11:52 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const symbols = ["♦", "♥", "♠", "♣"];
+  const numbers = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
+
+  function createCard() {
+    let randomSymbolsIndex = Math.floor(Math.random() * symbols.length);
+    let randomNumbersIndex = Math.floor(Math.random() * numbers.length);
+
+    const randomSymbol = symbols[randomSymbolsIndex];
+    const randomNumbers = numbers[randomNumbersIndex];
+
+    document.querySelector(".top-left").textContent = randomSymbol;
+    document.querySelector(".body").textContent = randomNumbers;
+    document.querySelector(".bottom-right").textContent = randomSymbol;
+
+    const card = document.querySelector(".card");
+    if (randomSymbol === "♥" || randomSymbol === "♦") {
+      card.style.color = "red";
+    } else {
+      card.style.color = "black";
+    }
+  }
+  createCard();
 };
