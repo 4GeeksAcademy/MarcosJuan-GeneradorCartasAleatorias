06133f72dcdafd3e37a1f76fd07f891f2d0bd1a2
Marcos Juan Martos Prados <martospradosmarcos@gmail.com>
2024-11-26 19:10:17 +0000

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   const symbols = ["♦", "♥", "♠", "♣"];
-  const numbers = [2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
+  const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
   function createCard() {
     let randomSymbolsIndex = Math.floor(Math.random() * symbols.length);
